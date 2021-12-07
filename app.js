a5d941daea720597b3acdb3d18883183cdd39e8b
tholakelebusi <tholakelebusi@gmail.com>
2021-12-07 10:46:57 -1100

    route with a name

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const app=express();
 app. listen(PORT,()=>
 console.log("server is rurning"))
 
-
+let bookList=[
+    "Power of Mind",
+    "The secret",
+    "The Secreate daughter"
+]
 
 // var express = require("express");
 // var app = express();
@@ -20,4 +24,9 @@ console.log("server is rurning"))
 app.get('/',(request,response)=>
 {
     response.send('Welcome to my special Api')
+})
+
+app.get('/books',(request,response)=>
+{
+    return response.json({allBooks:bookList})
 })
