a6e530c569b47311b98acc08ed447b6c238829e7
tholakelebusi <tholakelebusi@gmail.com>
2021-12-07 10:41:03 -1100

    first route

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-var express = require("express");
-var app = express();
-app.listen(3000, () => {
- console.log("Server running on port 3000");
-});
+import express from 'express'
+const PORT=3000;
+const app=express();
 
-app.get("/url", (req, res, next) => {
-    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
-   });
+app. listen(PORT,()=>
+console.log("server is rurning"))
+
+
+
+// var express = require("express");
+// var app = express();
+// app.listen(3000, () => {
+//  console.log("Server running on port 3000");
+// });
+
+// app.get("/url", (req, res, next) => {
+//     res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+//    });
+
+app.get('/',(request,response)=>
+{
+    response.send('Welcome to my special Api')
+})
